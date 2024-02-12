bceb44d3afa3952d2b00468e02ab42816202995d
makaylabreakspear <145589716+makaylabreakspear@users.noreply.github.com>
2024-02-12 10:33:28 -0500

    imgs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//   menu button
 document.getElementById("menu-toggle").addEventListener("click", function (){
     document.getElementById ("menu").classList.toggle("active");
 });
@@ -7,7 +8,7 @@ document.getElementById("menu-toggle").addEventListener("click", function (){
 
 
 console.log('hello world!');
-
+//   connecting the contentful
 var client = contentful.createClient({
     space: 'fdt9412ua609',
     accessToken: 'AOznWQA6p9nTvEus8d_87okFOAfNuTUzj31CTf3jAtM',
@@ -19,73 +20,49 @@ var client = contentful.createClient({
 
 
 
-
+//  getting all entries
   //   client is helper to connect to contentful
 //   getEntries is a method to get all entries from contentful
 //   entries is an object that contains all the entries
-client.getEntries().then(function (entries) {
+client.getEntries({content_type: 'moominBooks',}).then(function (entries) {
     console.log(entries);
     // for each entry run the function
+    
     entries.items.forEach(function (entry) {
         //  fields are the fields we named in contentful
+        //  naming the div section 
         var entryDiv = document.createElement('div');
       entryDiv.classList.add('entry-div');
+      //  img div
+      var imgDiv = document.createElement('div');
+      imgDiv.classList.add('img-div');
+      console.log(entry.fields.mainImage.fields.file.url);
+      var mainImage = document.createElement('img');
+      mainImage.src = entry.fields.mainImage.fields.file.url;
+      imgDiv.append(mainImage);
 
+      placeForContent.appendChild(imgDiv);
+      
+         //  creating the h2s
       if (entry.fields.name) {
         var name = document.createElement('h2');
         name.innerHTML = entry.fields.name;
         entryDiv.appendChild(name);
 
-        
+        //  creating the body text
             var description = document.createElement('p');
             description.innerHTML = entry.fields.description;
             entryDiv.appendChild(description);
-
+              //  creating the link
         var link = document.createElement('a');
         link.innerHTML = "link to " + entry.fields.name;
         link.href = entry.fields.link
         entryDiv.appendChild(link);
-
+               //  putting everything in the div in the html
         placeForContent.appendChild(entryDiv);
+
+
       }
       
     });
   });
-  
-
-
-
-
-
-
-
-
-
-
-
-
-// // var cars = [{type:"Fiat", model:"500", color:"white"}, 
-// //             {type:"Volvo", model:"XC90", color:"black"}]
-// // console.log(cars[0]); // to get all info on first car
-// // console.log(cars[0].type); // to get type of first car
-// // console.log(cars[1].model);
-
-// // cars.forEach (function (car) {
-// //     console.log("Car Type is " + car.type);
-// // });
-// // var students = [
-// //   {name:"Greg", grade:"A", subject:"AI", courses:[
-// //                                   {name:"web Tech"}, {name:"Logo"}
-// //                               ]}, 
-// //   {name:"Alice", grade:"B+", subject:"Marketing", courses:[
-// //                                   {name:"Biz"}, {name:"AI"}
-// //                               ]}
-// // ];
-//  console.log(students[0].name);
-
-// // students.forEach(function (student){
-// //   console.log ("student name is " + student.name);
-// //   student.courses.forEach(function (course){
-// //       console.log("Course is "+ course.name);
-// //   })
-// // });
